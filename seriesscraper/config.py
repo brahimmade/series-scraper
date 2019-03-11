db2a16be5c462bb954d2608f6753965c53a818fa
Dennis Effing <dennis.effing@hotmail.com>
2019-03-11 18:35:31 +0100

    Fix config file path on Linux.

--- seriesscraper/config.py
+++ seriesscraper/config.py
@@ -1,18 +1,18 @@
 import logging
 import sys
+from pathlib import Path
 
-__config_file = '..\\config.yaml'
+__config_file = Path(__file__).parents[1] / 'config.yaml'  # root dir of project
 
 
-def load_config(file: str = __config_file) -> dict:
+def load_config(config_file: str = __config_file) -> dict:
     import yaml
-    import os
 
-    file_dir = os.path.dirname(__file__)
-    if not os.path.isfile(os.path.join(file_dir, file)) or os.path.getsize(os.path.join(file_dir, file)) == 0:
+    config_file_path = Path(config_file)
+    if not config_file_path.is_file() or config_file_path.stat().st_size == 0:
         logging.error('Please provide a configuration file before starting the crawler.')
         sys.exit(0)
 
-    with open(os.path.join(file_dir, file), 'r') as file_handle:
+    with open(config_file, 'r') as file_handle:
         content = yaml.load(file_handle)  # throws YAMLError
         return content
